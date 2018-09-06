1a404feab53e66c1f0d87f38e5089e927e318b2e
Attila Rajmund Nohl <attila.nohl@erlang-solutions.com>
2018-09-06 15:16:51 +0200

    Try to use password-encrypted private key.

--- travis_script.sh
+++ travis_script.sh
@@ -2,9 +2,9 @@
 set -x
 
 eval "$(ssh-agent -s)"
-echo "$MY_SECRET_ENV" | ssh-add /dev/stdin
-#chmod 600 scripts/updater.key
+chmod 600 scripts/updater.key
 #ssh-add scripts/updater.key
+sshpass -p "$SSH_KEY_PASSWORD" ssh-add scripts/updater.key
 
 export PROJECT_NAME=`echo $TRAVIS_REPO_SLUG | sed 's,/,-,g'`
 export REBAR_COLOR=none
